16d3a938284f49e7b23fe6c8ab1b7c676f721eb9
Divy Patel <125264776+Divy0409@users.noreply.github.com>
2024-07-11 03:43:30 -0400

    update frontend

--- ChatBots_FE/src/App.js
+++ ChatBots_FE/src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
-import Chatbots from './components/chatbots';  // Adjust path as per your file structure
+// src/App.js
+
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigationType, useLocation } from 'react-router-dom';
+import Home from './components/home';
+import Chatbots from './components/chatbots';
+import About from './components/about';
 
 function App() {
+  const action = useNavigationType();
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  useEffect(() => {
+    if (action !== "POP") {
+      window.scrollTo(0, 0);
+    }
+  }, [action, pathname]);
+
+  useEffect(() => {
+    let title = "";
+    let metaDescription = "";
+
+    switch (pathname) {
+      case "/":
+        title = "Home";
+        metaDescription = "Welcome to the Chatbots Homepage";
+        break;
+      case "/chatbots":
+        title = "Chatbots";
+        metaDescription = "List of Chatbots";
+        break;
+      case "/about":
+        title = "About Us";
+        metaDescription = "Learn more about us";
+        break;
+      default:
+        title = "Chatbot Application";
+        metaDescription = "Chatbot Application";
+        break;
+    }
+
+    if (title) {
+      document.title = title;
+    }
+
+    if (metaDescription) {
+      const metaDescriptionTag = document.querySelector('head > meta[name="description"]');
+      if (metaDescriptionTag) {
+        metaDescriptionTag.content = metaDescription;
+      }
+    }
+  }, [pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Chatbots />
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/chatbots" element={<Chatbots />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
